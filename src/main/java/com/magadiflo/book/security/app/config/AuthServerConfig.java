79b7c4f7dcdabf75c8543c1b558c1f5d024fedee
Martín <magadiflo@gmail.com>
2023-06-13 19:34:33 -0500

    Implementación de pizarra (blackboarding) con un JdbcTokenStore

--- src/main/java/com/magadiflo/book/security/app/config/AuthServerConfig.java
+++ src/main/java/com/magadiflo/book/security/app/config/AuthServerConfig.java
@@ -1,13 +1,17 @@
 package com.magadiflo.book.security.app.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @EnableAuthorizationServer
 @Configuration
@@ -16,9 +20,13 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(this.authenticationManager);
+        endpoints.authenticationManager(this.authenticationManager)
+                .tokenStore(this.tokenStore());
     }
 
     @Override
@@ -27,14 +35,11 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .withClient("client")
                 .secret("secret")
                 .authorizedGrantTypes("password", "refresh_token")
-                .scopes("read")
-                .and()
-                .withClient("resourceserver")   // Agrega un conjunto de credenciales para que el servidor
-                .secret("resourceserversecret");// de recursos las use al llamar al endpoint /oauth/check_token
+                .scopes("read");
     }
 
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) {
-        security.checkTokenAccess("isAuthenticated()"); //<--- Especifica la condición en la que podemos llamar al punto final /check_token
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(this.dataSource);
     }
 }
